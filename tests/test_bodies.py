8bf185c1b27adfc9cc9420ccb85a88765ba92c5f
AbdelrahmanOthman33 <Abdelrahmanothman30@gmail.com>
2025-07-09 11:51:31 -0700

    Delete tests directory

--- tests/test_bodies.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from nbody.bodies import CelestialBody
-
-def test_celestial_body_creation():
-    body = CelestialBody("Test", 1.0, [1,2,3], [0.1, 0.2, 0.3])
-    assert body.name == "Test"
-    assert body.mass == 1.0
-    assert len(body.position) == 3
-    assert len(body.velocity) == 3
